5384ad81fb3677817a027738f4f70ff49ba2ee0d
Tom Silver <tomlikefuturam@gmail.com>
2014-05-15 01:19:23 +0800

    updated

--- iOS7/Fashionista/Classes/Views/iPhone/Model/Global.h
+++ iOS7/Fashionista/Classes/Views/iPhone/Model/Global.h
@@ -14,8 +14,25 @@
 #define API_BASE_VERSION    @"/api/v1"
 
 #define API_USER_LOGIN      @"%@%@user/add"
-
 #define FB_PROFILE_ICON     @"http://graph.facebook.com/%@/picture?type=large"
+
+#define API_IMAGE_LATEST @"http://pixcell8.com/api/v1/image/latest/"
+#define API_IMAGE_SEARCH @"http://pixcell8.com/api/v1/image/search/"
+#define API_IMAGE_UPDATE_INFO @"http://pixcell8.com/api/v1/image/edit/"
+#define API_IMAGE_UPLOAD @"http://pixcell8.com/api/v1/image/upload"
+#define API_IMAGE_UPLOAD2 @"http://pixcell8.com/api/v1/image/info"
+
+#define API_IMAGE_USER @"http://pixcell8.com/api/v1/image/%d/%d/user/%d"
+#define API_IMAGE_USER_SALES @"http://pixcell8.com/api/v1/sales/%d/%d/user/%d"
+
+#define API_IMAGE_USER_SALES_BYMONTH @"http://pixcell8.com/api/v1/sales/month/%d?month=%d&year=%d"
+
+#define API_IMAGE_USER_PURCHASE @"http://pixcell8.com/api/v1/purchase/user/"
+#define API_IMAGE_PROFILE_UPLOAD @"http://pixcell8.com/api/v1/profile/picture/upload"
+
+#define URL_IMAGE_PATH @"http://pixcell8.com/uploads/media/user/images/"
+#define URL_IMAGE_PROFILE_PATH @"http://pixcell8.com/uploads/media/user/profile/"
+
 
 #endif
 
