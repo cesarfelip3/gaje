cf877e5b03c1e4cfcc7e5a423741f2159704913d
hello <hello@hellodeMacBook-Air.local>
2014-12-31 08:19:35 +0800

    local table

---
+++ iOS7.Universal/Fashionista/Classes/Views/iPhone/Model/Entity/Setting.h
@@ -0,0 +1,21 @@
+//
+//  Setting.h
+//  Gaje
+//
+//  Created by hello on 14-12-31.
+//  Copyright (c) 2014年 AppDesignVault. All rights reserved.
+//
+
+#import "Model.h"
+
+@interface Setting : Model
+
++ (id)getInstance;
+
+- (BOOL)addItem:(NSString *)key Value:(NSString *)value;
+- (BOOL)updateItem:(NSString *)key Value:(NSString *)value;
+- (BOOL)removeItem:(NSString *)key;
+
+- (BOOL)getItem:(NSString *)key;
+
+@end
---
+++ iOS7.Universal/Fashionista/Classes/Views/iPhone/Model/Entity/Setting.m
@@ -0,0 +1,23 @@
+//
+//  Setting.m
+//  Gaje
+//
+//  Created by hello on 14-12-31.
+//  Copyright (c) 2014年 AppDesignVault. All rights reserved.
+//
+
+#import "Setting.h"
+
+@implementation Setting
+
++ (id)getInstance {
+    static Setting *instance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        instance = [[self alloc] init];
+        
+    });
+    return instance;
+}
+
+@end
